d119ca9fe41b21257001122687dcc91b0e3f0101
Pedro <pedroreisrodrigues2905@gmail.com>
2022-05-20 01:54:28 -0300

    PaginaFinalizada

--- src/pages/Publicacao/index.js
+++ src/pages/Publicacao/index.js
@@ -19,15 +19,47 @@ export default function publicacao(){
         </header>
         <section className="Faixa-1">
             <div className="esquerda"> 
-                <div>
+                <div className="card-princ">
+                 <img width="290px" height="250px" src="./images/image 5.png"/> 
+                <div className="info-card">
+                <h1> Nome: <span className="info-p"> Rex </span> </h1>  
+                 <h1> Raça: <span className="info-p">Vira Lata</span> </h1>  
+                 <h1> Sexo:<span className="info-p"> Macho </span> </h1>
+                 <h1> Localização: <span className="info-p"> Cidade Dutra </span> </h1>
+                 <h1> Meio de Contato: <span className="info-p"> @xyz </span> </h1>
+                 <h1> Intereçados: <span className="info-p">27</span> </h1>
+                 </div>                                
+                 </div>
+                 <div className="finally">
+                <h3> Coelho Bege de 2 anos de idade </h3>
+                <Link className="final" to='/Posts'>
+                      Editar
+                    </Link>   
 
-                </div>            
+                </div>
             </div>
 
-            <div> 
-                
-            </div>            
+
+            <div className="esquerda"> 
+                <div className="card-princ">
+                <img width="300px" src="./images/image 13.png"/> 
+                <div className="info-card">
+                <h1> Nome: <span className="info-p"> Tel </span> </h1>  
+                 <h1> Raça: <span className="info-p">Vira Lata</span> </h1>  
+                 <h1> Sexo:<span className="info-p"> Macho </span> </h1>
+                 <h1> Localização: <span className="info-p"> Cidade Dutra </span> </h1>
+                 <h1> Meio de Contato: <span className="info-p"> @xyz </span> </h1>
+                 <h1> Intereçados: <span className="info-p">27</span> </h1>
+                 </div>                                
+                 </div>
+                 <div className="finally">
+                <h3> Coelho Bege de 2 anos de idade </h3>
+                <Link className="final" to='/Posts'>
+                      Editar
+                    </Link>                    <img src="./Delete.png"/>                                       
+                </div>       
+            </div>      
         </section>        
         </main>     
     )
-}
+}   
