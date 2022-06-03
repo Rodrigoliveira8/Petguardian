d16af3fcd07953e92d1133e6bc74b9cec3aa069a
Pedro <pedroreisrodrigues2905@gmail.com>
2022-06-02 23:26:48 -0300

    ddl e dml configurados

---
+++ Banco de dados/dml.sql
@@ -0,0 +1,67 @@
+--Feed
+SELECT id_pet           id,
+       id_usuario       Usuário,
+       nm_pet           Nome do Pet,
+       ds_raca          Raca,
+       ds_localizacao   Localizacao,
+       ds_sexo          Sexo,
+       ds_pet           Descricao,
+       img_pet          Imagem,
+       ds_contato       Contato
+FROM tb_pet;
+
+
+--Cadastrar Conta
+Insert INTO tb_usuario (nm_usuario,ds_email,ds_senha,dt_nascimento,ds_telefone,ds_endereco)
+VALUES('Pedro', 'pedro@pedro@gmail', '1234', '2006-05-29', '(11) 1234-1234', 'Rua dos Loucos 55');
+
+--Login
+SELECT id_usuario       id,
+       nm_usuario       Nome,
+FROM tb_usuario
+WHERE ds_email          = 'admin@admin.com.br'
+AND   ds_senha          ='1234';
+
+
+--Criar Post
+Insert INTO tb_pet(id_usuario,nm_pet,ds_raca,ds_localizacao,ds_sexo,ds_pet,ds_contato)
+VALUES (1,'Léu', 'PitBull', 'Rua dos Loucos 55', 'Macho', 'ruf ruf', '(11) 1324-1324');
+
+
+
+--Cadastrar Imagem
+UPDATE tb_pet
+SET img_pet             ='/storage/fotos/adsf.jp'
+WHERE id_pet            =1;
+
+
+
+--Editar Post
+UPDATE tb_pet
+SET    nm_pet           ='Léu',
+       ds_raca          ='PitBull',
+       ds_localizacao   ='Rua dos Loucos 55',
+       ds_sexo          ='Macho',
+       ds_pet           ='ruf ruf',
+       ds_contato       ='(11) 1234-1234'
+WHERE id_pet = 1;
+
+
+
+--Deletar Post
+DELETE FROM tb_pet
+WHERE id_pet = 1;
+
+
+
+--Listar Post
+SELECT id_pet           id,
+       id_usuario       Usuário,
+       nm_pet           Nome do Pet,
+       ds_raca          Raca,
+       ds_localizacao   Localizacao,
+       ds_sexo          Sexo,
+       ds_pet           Descricao,
+       img_pet          Imagem,
+       ds_contato       Contato
+WHERE id_usuario        =1;
